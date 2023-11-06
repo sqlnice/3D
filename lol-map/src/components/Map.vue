f557b49a371faa258524695946a7a24c6a99533a
时启龙 <shiqilong@beyondcent.com>
2023-11-06 17:52:46 +0800

    feat: 添加静态资源

---
+++ lol-map/src/components/Map.vue
@@ -0,0 +1,23 @@
+<script setup>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+  L.marker([51.5, -0.09]).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup()
+})
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
